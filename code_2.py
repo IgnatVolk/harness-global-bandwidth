e1e740ac174be76bc9c0d599bde9780468817323
IgnatVolk <142455386+IgnatVolk@users.noreply.github.com>
2024-01-22 19:20:59 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,37 @@
+        }
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+
+    }
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+        }
+}
+}
+    public static void main(String[] args) {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+        }
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+
+    }
+        }        for (int item : data) {
+        }
+        int[] data = new int[10];
+
+}
+    }
+            System.out.println("Random Number: " + item);
+        }
+        for (int i = 0; i < 10; i++) {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+public class RandomDataGenerator {
